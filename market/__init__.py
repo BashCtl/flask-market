38b2802ed71aa9ddcdc943314657f661c0a3714d
darko <ki14.skakun@gmail.com>
2023-06-27 14:48:26 +0300

    initial commit

---
+++ market/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///../instance/market.db"
+app.config['SECRET_KEY'] = "fc427960acd3ac08a126f4d0"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+from market import routers

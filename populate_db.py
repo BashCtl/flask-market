5d6ab49e21f5dc452993f59e23675503b8272e1f
darko <ki14.skakun@gmail.com>
2023-09-13 16:50:40 +0300

    add git ignore

--- populate_db.py
+++ populate_db.py
@@ -1,13 +1,13 @@
 from market.models import db, User, Item
 from market import app
 
-u1 = User(username="jsc", password_hash="123456", email_address="jsc@jsc.com")
-i1 = Item(name="Iphone 12", barcode="234987645912", price=840, description="Iphone 12 description")
-i2 = Item(name="Laptop Lenovo Yoga", barcode="764987645912", price=750, description="Lenovo Yoga description")
-i3 = Item(name="Keyboard", barcode="894081645912", price=85, description="Keyboard description")
+# u1 = User(username="jsc", password_hash="123456", email_address="jsc@jsc.com")
+# i1 = Item(name="Iphone 12", barcode="234987645912", price=840, description="Iphone 12 description")
+# i2 = Item(name="Laptop Lenovo Yoga", barcode="764987645912", price=750, description="Lenovo Yoga description")
+# i3 = Item(name="Keyboard", barcode="894081645912", price=85, description="Keyboard description")
 
 with app.app_context():
-    # db.create_all()
+    db.create_all()
     # db.session.add(i1)
     # db.session.add(i2)
     # db.session.add(i3)

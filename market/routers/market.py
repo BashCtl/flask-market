ea70e1f62af72858b5fea8362a94008a89587aa1
darko <ki14.skakun@gmail.com>
2023-10-12 15:14:21 +0300

    added admin required

--- market/routers/market.py
+++ market/routers/market.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_login import login_required
 from market.services.market_service import MarketService
+from market.decorators import admin_required
 
 market = Blueprint("market", __name__)
 
@@ -19,23 +20,27 @@ def market_page():
 
 @market.route("/market/admin")
 @login_required
+@admin_required
 def admin_page():
     return MarketService.admin()
 
 
 @market.route("/market/admin/new_item", methods=["GET", "POST"])
 @login_required
+@admin_required
 def new_item():
     return MarketService.new_item()
 
 
 @market.route("/market/admin/edit_item/<int:item_id>", methods=["GET", "POST"])
 @login_required
+@admin_required
 def edit_item(item_id):
     return MarketService.edit_item(item_id)
 
 
 @market.route("/market/admin/delete_item/<int:item_id>", methods=["GET"])
 @login_required
+@admin_required
 def delete_item(item_id):
     return MarketService.delete_item(item_id)
